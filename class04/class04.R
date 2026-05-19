ab533b5ca40b24d82683fac7f607cf7024ab9293
Harshitha Palacharla <hpalacharla@ucsd.edu>
2026-05-19 12:43:40 -0700

    Add class 4 to 7

---
+++ class04/class04.R
@@ -0,0 +1,8 @@
+#' ---
+#' title: "Class4: Intro to R"
+#' author: "Harshitha Palacharla (PID: A17775151)"
+#' ---
+
+# This is just a text file
+x <- 1:50
+plot(x, sin(x), typ="l", col="blue", lwd="3")
